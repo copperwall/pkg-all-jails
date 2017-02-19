c25d80ce54c7549a01e52e3041453ae82654b060
Chris Opperwall <chriso@ifixit.com>
2017-02-19 01:36:14 -0800

    Fix variable typo in for loop
    I accidentally included the `$` in the for loop iterator variable, which caused
    the script to fail. This also adds the executable permission to the file so
    that a user can run it like `./pkg-all-jails.sh`.

--- pkg-all-jails.sh
+++ pkg-all-jails.sh
@@ -4,7 +4,7 @@
 JAILS="$(jls | awk 'NR>1 {print $1}')"
 
 # Run pkg update/upgrade on all JIDs.
-for $JAIL in $JAILS; do
+for JAIL in $JAILS; do
    sudo jexec $JAIL pkg update
    sudo jexec $JAIL pkg upgrade
 done
